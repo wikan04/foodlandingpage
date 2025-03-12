f10aed05889e66d8ff95b32e2991711d7c99dea0
wikan04 <wikanworldstar@gmail.com>
2025-03-13 02:51:15 +0700

    Fixing Lokasi Text

--- app/Hero/page.tsx
+++ app/Hero/page.tsx
@@ -5,8 +5,8 @@ export default function Heropage() {
       style={{ backgroundImage: "url('/background-hero.jpg')" }}
     >
       <div className="mx-auto flex flex-col items-center px-5 py-24 md:flex-row">
-        <div className="mb-16 flex flex-col  text-center md:mb-0 md:w-1/2 md:items-start md:pr-16 md:text-left lg:flex-grow lg:pr-24">
-          <h1 className="title-font mb-4 text-3xl font-bold text-sushi sm:text-5xl">
+        <div className="mb-16 flex flex-col text-left md:mb-0 md:w-1/2 md:items-start md:pr-16  lg:flex-grow lg:pr-24">
+          <h1 className="title-font mb-4 text-4xl font-bold text-sushi sm:text-5xl">
             Good Food Us <br className="hidden lg:inline-block" />
             Good Mood
           </h1>
