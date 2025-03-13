091d0b1e520af477e6894d7e26bbc7b34ba6ebad
wikan04 <wikanworldstar@gmail.com>
2025-03-13 18:56:08 +0700

    Fixing Responsive On Laptop and Tablet

--- app/Hero/page.tsx
+++ app/Hero/page.tsx
@@ -6,7 +6,7 @@ export default function Heropage() {
     >
       <div className="mx-auto flex flex-col items-center px-5 py-24 md:flex-row">
         <div className="mb-16 flex flex-col text-left md:mb-0 md:w-1/2 md:items-start md:pr-16  lg:flex-grow lg:pr-24">
-          <h1 className="title-font mb-4 text-4xl font-semibold text-sushi sm:text-5xl">
+          <h1 className="title-font mb-4 text-4xl font-semibold text-sushi lg:text-4xl xl:text-5xl">
             Good Food Us <br className="hidden lg:inline-block" />
             Good Mood
           </h1>
@@ -22,16 +22,16 @@ export default function Heropage() {
             conservative position: Not to waste food, and not to throw away a
             lot of the food that we buy.
           </p>
-          <div className="flex justify-start items-start">
-            <button className="inline-flex rounded-full border-0 bg-sushi px-6 py-2 text-sm lg:text-lg text-white font-bold focus:outline-none">
+          <div className="flex justify-start items-start text-left">
+            <button className="inline-flex rounded-full border-0 bg-sushi px-6 py-2 text-xs lg:text-md xl:text-lg text-white font-bold focus:outline-none">
               Daftar Sekarang
             </button>
-            <button className="ml-4 inline-flex rounded-full border-0 bg-gray-100 px-6 py-2 text-sm lg:text-lg text-gray-700 font-bold focus:outline-none">
+            <button className="ml-4 inline-flex rounded-full border-0 bg-gray-100 px-6 py-2 text-xs lg:text-md xl:text-lg text-gray-700 font-bold focus:outline-none">
               About Us
             </button>
           </div>
         </div>
-        <div className="hidden md:block w-1/2 lg:w-full lg:max-w-3xl">
+        <div className="hidden md:block w-1/2 lg:w-2/3 xl:w-1/2 2xl:w-full lg:max-w-3xl">
           <img
             className="rounded object-cover object-center"
             alt="hero"
